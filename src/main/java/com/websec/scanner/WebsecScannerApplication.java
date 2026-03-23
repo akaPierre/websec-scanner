16f80321a46225442b150403a8ccfeeb68c7f626
akaPierre <pierre.ap32@gmail.com>
2026-03-23 18:57:46 -0300

    feat: initial project setup - websec-scanner

---
+++ src/main/java/com/websec/scanner/WebsecScannerApplication.java
@@ -0,0 +1,13 @@
+package com.websec.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsecScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsecScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websec/scanner/WebsecScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websec.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsecScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
